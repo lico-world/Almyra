e6316fffe11d20f175a08637a9f776e462fca8b1
lico <107700554+lico-world@users.noreply.github.com>
2024-01-29 19:39:07 +0200

    Initialisation of the ModToolsMaterial enum to implements tools materials

---
+++ src/main/java/net/lico/almyra/item/ModToolMaterial.java
@@ -0,0 +1,64 @@
+package net.lico.almyra.item;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterial implements ToolMaterial
+{
+	;
+
+	private final int miningLevel;
+	private final int itemDurability;
+	private final float miningSpeed;
+	private final float attackDamage;
+	private final int enchantability;
+	private final Supplier<Ingredient> repairIngredients;
+
+	ModToolMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredients)
+	{
+		this.miningLevel = miningLevel;
+		this.itemDurability = itemDurability;
+		this.miningSpeed = miningSpeed;
+		this.attackDamage = attackDamage;
+		this.enchantability = enchantability;
+		this.repairIngredients = repairIngredients;
+	}
+
+	@Override
+	public int getDurability()
+	{
+		return this.itemDurability;
+	}
+
+	@Override
+	public float getMiningSpeedMultiplier()
+	{
+		return this.miningSpeed;
+	}
+
+	@Override
+	public float getAttackDamage()
+	{
+		return this.attackDamage;
+	}
+
+	@Override
+	public int getMiningLevel()
+	{
+		return this.miningLevel;
+	}
+
+	@Override
+	public int getEnchantability()
+	{
+		return this.enchantability;
+	}
+
+	@Override
+	public Ingredient getRepairIngredient()
+	{
+		return this.repairIngredients.get();
+	}
+}
